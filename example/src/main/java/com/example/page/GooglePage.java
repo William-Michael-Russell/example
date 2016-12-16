0265d3cfcc0af5d42b38e97684069d37706ec0d2
William Russell <William.Russell@katch.com>
2016-12-16 15:56:14 -0800

    added two examples

---
+++ example/src/main/java/com/example/page/GooglePage.java
@@ -0,0 +1,20 @@
+package com.example.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Created by william.russell on 12/16/16.
+ */
+public class GooglePage {
+
+    @FindBy(id = "lst-ib")
+    public WebElement searchBox;
+
+
+    public GooglePage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+    }
+}
